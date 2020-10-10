af633fc124d54263904b44418c1323bcfcb301fe
Sixto Alcoba <sixtoalcobabanqueri@gmail.com>
2020-10-10 11:31:05 +0200

    selection working

---
+++ Sohan_project/Assets/Manager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour
+{
+    public GameObject cursor;
+ 
+    public GameObject[] targets; 
+
+    Vector3 worldPosition; 
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        //Debug.Log("mouse position  : " + worldPosition.x + " , " + worldPosition.y);
+
+        mousePos.z = 0;//Camera.main.nearClipPlane;
+        worldPosition = Camera.main.ScreenToWorldPoint(mousePos);
+        cursor.transform.position = new Vector3( worldPosition.x, worldPosition.y,0)  ;
+        //Debug.Log("mouse position on scene : "+ worldPosition.x + " , " + worldPosition.y );  
+
+        foreach (GameObject targetgo in targets)
+        { 
+            if ((targetgo.transform.position - cursor.transform.position).magnitude < 1.0f)
+            {
+                targetgo.GetComponent<Target>().TargetSelected();
+            }
+            else
+            {
+                if (targetgo.GetComponent<Target>().ObjectSelected)
+                {
+                    targetgo.GetComponent<Target>().TargetUnselected();
+
+                }
+            }
+            
+            
+
+        }
+
+
+
+    }
+
+ 
+
+
+}
---
+++ Sohan_project/Assets/Target.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+
+    public bool ObjectSelected = false; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TargetSelected()
+    {
+        Material mate=gameObject.GetComponent<Renderer>().material; 
+        mate.color = new Color(0.5f, 1, 1); //C#
+        ObjectSelected = true; 
+    }
+
+    public void TargetUnselected()
+    {
+        Material mate = gameObject.GetComponent<Renderer>().material;
+        mate.color = new Color(0, 0, 0); //C#
+        ObjectSelected = false;
+
+    }
+}
